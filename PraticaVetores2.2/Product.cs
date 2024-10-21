ca1d7b0f0cc54d346a9a6db89325e7bc2f735877
Vinícius Aquino <vinicius.aquino@estudante.ufcg.edu.br>
2024-10-21 20:47:28 -0300

    Add files via upload

---
+++ PraticaVetores2.2/Product.cs
@@ -0,0 +1,6 @@
+﻿namespace PraticaVetores2._2 {
+    internal class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ PraticaVetores2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+namespace PraticaVetores2._2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("How many products? ");
+            int n = int.Parse(Console.ReadLine());
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++) {
+                Console.Write($"Name {i + 1}º Product:");
+                string name = Console.ReadLine();
+                Console.Write($"Price {i + 1}º Product:");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
+            }
+            double sum = 0.0;
+            for (int i = 0; i < n; i++) {
+                sum += vect[i].Price;
+            }
+            double avg = sum / n;
+            Console.WriteLine($"Average Price: {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
